3fb04ad5b8e52b4bf118200668d88ed93650e9bd
Wesley Klop <wesley19097@gmail.com>
2018-03-05 15:43:27 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({ width: 800, height: 600 })
+
+  // and load the index.html of the app.
+  win.loadURL(URL.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }))
+}
+
+app.on('ready', createWindow)
