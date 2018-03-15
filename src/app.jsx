87d55e4921dfc374c02c1f5feaa979389e8dfa9f
Wesley Klop <wesley19097@gmail.com>
2018-03-15 13:56:42 +0100

    Remove the old components and import the new ones
    (cherry picked from commit 26089119fcf026462e9b0ce4230c665a640b6d16)

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,79 @@
 import React from 'react'
 import { render } from 'react-dom'
-import './style.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Card, Col, Icon, Layout, Menu, Row } from 'antd'
+import 'antd/dist/antd.css'
+import './main.css'
+import Dashboard from './components/pages/DashboardPage';
+import Sidebar from './components/Sidebar';
 
-const App = () => <div>Hello!</div>
+const { Header, Content } = Layout
+const { Meta } = Card
 
-render(<App />, document.querySelector('#target'))
+
+const App = () => (
+  <Layout>
+    <Sidebar />
+    <Switch>
+      <Route exact path="/receipt" component={ReceiptListPage} />
+      <Route path="/receipt/:id" render={({ match }) => match.params.id} />
+      <Route component={Dashboard} />
+    </Switch>
+  </Layout>
+)
+
+const Receipt = () => (
+  <Card
+    className="receipt"
+    hoverable
+    cover={<img className="cover" alt="example"
+                src="https://d85ecz8votkqa.cloudfront.net/support/help_center/Print_Payment_Receipt.JPG" />}
+    actions={['€5,00', 'Sergi']}
+  >
+    <Meta
+      title="Een bon"
+      description="This is the description"
+    />
+  </Card>
+)
+
+const ReceiptListPage = () => (
+  <Layout>
+    <Header className="header">
+      <Menu className="menu" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item>
+          <Icon type="file-add" />
+          <span>New Receipt</span>
+        </Menu.Item>
+      </Menu>
+    </Header>
+    <Content className="content">
+      <Row gutter={16}>
+        <Col xxl={6} xl={8} md={12} sm={24}>
+          <Receipt />
+        </Col>
+        <Col xxl={6} xl={8} md={12} sm={24}>
+          <Receipt />
+        </Col>
+        <Col xxl={6} xl={8} md={12} sm={24}>
+          <Receipt />
+        </Col>
+        <Col xxl={6} xl={8} md={12} sm={24}>
+          <Receipt />
+        </Col>
+        <Col xxl={6} xl={8} md={12} sm={24}>
+          <Receipt />
+        </Col>
+        <Col xxl={6} xl={8} md={12} sm={24}>
+          <Receipt />
+        </Col>
+      </Row>
+    </Content>
+  </Layout>
+)
+
+render(
+  <Router>
+    <App />
+  </Router>
+  , document.querySelector('#target'))
